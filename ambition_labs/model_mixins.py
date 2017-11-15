e57d8e842db8fec43721a699e56bdde4958f68de
erikvw <ew2789@gmail.com>
2017-11-15 10:30:49 +0200

    fix tests, fix import error on panel

--- ambition_labs/model_mixins.py
+++ ambition_labs/model_mixins.py
@@ -1,43 +1,43 @@
-from django.db import models
-
-from edc_lab.model_mixins.requisition import RequisitionIdentifierMixin
-from edc_lab.model_mixins.requisition import RequisitionModelMixin, RequisitionStatusMixin
-from edc_map.site_mappers import site_mappers
-from edc_metadata.model_mixins.updates import UpdatesRequisitionMetadataModelMixin
-from edc_offstudy.model_mixins import OffstudyMixin
-from edc_search.model_mixins import SearchSlugManager
-from edc_visit_tracking.managers import CrfModelManager as VisitTrackingCrfModelManager
-from edc_visit_tracking.model_mixins import CrfModelMixin as VisitTrackingCrfModelMixin
-from edc_visit_tracking.model_mixins import PreviousVisitModelMixin
-
-
-class Manager(VisitTrackingCrfModelManager, SearchSlugManager):
-    pass
-
-# TODO Is any of this used??
-
-
-class SubjectRequisitionModelMixin(
-        RequisitionModelMixin, RequisitionStatusMixin, RequisitionIdentifierMixin,
-        VisitTrackingCrfModelMixin, OffstudyMixin,
-        PreviousVisitModelMixin,
-        UpdatesRequisitionMetadataModelMixin, models.Model):
-
-    objects = Manager()
-
-    def save(self, *args, **kwargs):
-        self.study_site = site_mappers.current_map_code
-        self.study_site_name = site_mappers.current_map_area
-        super().save(*args, **kwargs)
-
-    def get_search_slug_fields(self):
-        fields = [
-            'requisition_identifier',
-            'human_readable_identifier',
-            'panel_name',
-            'panel_object.abbreviation',
-            'identifier_prefix']
-        return fields
-
-    class Meta:
-        abstract = True
+# from django.db import models
+#
+# from edc_lab.model_mixins.requisition import RequisitionIdentifierMixin
+# from edc_lab.model_mixins.requisition import RequisitionModelMixin, RequisitionStatusMixin
+# from edc_map.site_mappers import site_mappers
+# from edc_metadata.model_mixins.updates import UpdatesRequisitionMetadataModelMixin
+# from edc_offstudy.model_mixins import OffstudyMixin
+# from edc_search.model_mixins import SearchSlugManager
+# from edc_visit_tracking.managers import CrfModelManager as VisitTrackingCrfModelManager
+# from edc_visit_tracking.model_mixins import CrfModelMixin as VisitTrackingCrfModelMixin
+# from edc_visit_tracking.model_mixins import PreviousVisitModelMixin
+#
+#
+# class Manager(VisitTrackingCrfModelManager, SearchSlugManager):
+#     pass
+#
+# # TODO Is any of this used??
+#
+#
+# class SubjectRequisitionModelMixin(
+#         RequisitionModelMixin, RequisitionStatusMixin, RequisitionIdentifierMixin,
+#         VisitTrackingCrfModelMixin, OffstudyMixin,
+#         PreviousVisitModelMixin,
+#         UpdatesRequisitionMetadataModelMixin, models.Model):
+#
+#     objects = Manager()
+#
+#     def save(self, *args, **kwargs):
+#         self.study_site = site_mappers.current_map_code
+#         self.study_site_name = site_mappers.current_map_area
+#         super().save(*args, **kwargs)
+#
+#     def get_search_slug_fields(self):
+#         fields = [
+#             'requisition_identifier',
+#             'human_readable_identifier',
+#             'panel_name',
+#             'panel_object.abbreviation',
+#             'identifier_prefix']
+#         return fields
+#
+#     class Meta:
+#         abstract = True
--- ambition_labs/tests/test_labs.py
+++ ambition_labs/tests/test_labs.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from edc_lab.site_labs import site_labs
 
 from ..labs import lab_profile
-from .models import SubjectRequisition
 
 
 class TestLabs(TestCase):
@@ -12,7 +11,7 @@ class TestLabs(TestCase):
         site_labs.loaded = False
         site_labs.register(
             lab_profile=lab_profile,
-            requisition_model='ambition_lab.subjectrequisition')
+            requisition_model='ambition_subject.subjectrequisition')
 
     def test_(self):
         obj = site_labs.get(lab_profile_name='ambition_subject')
@@ -20,9 +19,10 @@ class TestLabs(TestCase):
 
     def test_lab_profile_model(self):
         obj = site_labs.get(lab_profile_name='ambition_subject')
-        self.assertEqual('ambition_lab.subjectrequisition',
+        self.assertEqual('ambition_subject.subjectrequisition',
                          obj.requisition_model)
 
     def test_panel_model(self):
         for panel in site_labs.get(lab_profile_name='ambition_subject').panels.values():
-            self.assertEqual(panel.model, 'ambition_lab.subjectrequisition')
+            self.assertEqual(
+                panel.model, 'ambition_subject.subjectrequisition')
