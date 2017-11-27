d5d97c506f00a3e82f908d64aec4c04320297228
erikvw <ew2789@gmail.com>
2017-11-27 07:02:33 +0200

    add csf_stop_panel to init

--- ambition_labs/__init__.py
+++ ambition_labs/__init__.py
@@ -3,4 +3,4 @@ from .labs import chemistry_alt_panel, chemistry_panel, csf_chemistry_panel
 from .labs import serum_panel, plasma_buffycoat_panel, csf_pkpd_panel, wb_panel
 from .labs import pk_plasma_panel_t2, pk_plasma_panel_t4, pk_plasma_panel_t7
 from .labs import pk_plasma_panel_t12, pk_plasma_panel_t23
-from .labs import qpcr_blood_panel, qpcr_csf_panel
+from .labs import qpcr_blood_panel, qpcr_csf_panel, csf_stop_panel
