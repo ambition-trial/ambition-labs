43ff339546214b1f03c8a22a201a95f616283504
erikvw <ew2789@gmail.com>
2018-04-17 12:46:45 +0200

    requirements,travis

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email='leslie.kep@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/botswana-harvard/ambition-labs',
+    url='https://github.com/ambition-study/ambition-labs',
     license='GPL license, see LICENSE',
     description='ambition lab modules',
     long_description=README,
