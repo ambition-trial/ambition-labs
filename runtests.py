cd357ba73c710c2b7f5d72b9b02aa4abc30412fc
erikvw <ew2789@gmail.com>
2019-04-01 07:59:43 -0500

    tox

---
+++ runtests.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import django
+import logging
+import os
+import sys
+
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+from edc_test_utils import DefaultTestSettings
+from os.path import abspath, dirname, join
+
+
+app_name = 'ambition_labs'
+base_dir = dirname(abspath(__file__))
+
+DEFAULT_SETTINGS = DefaultTestSettings(
+    calling_file=__file__,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
+    SUBJECT_VISIT_MODEL="ambition_subject.subjectvisit",
+    EDC_BOOTSTRAP=3,
+    RANDOMIZATION_LIST_PATH=join(
+        base_dir, app_name, "tests", "test_randomization_list.csv"),
+    INSTALLED_APPS=[
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'django.contrib.sites',
+        'django_crypto_fields.apps.AppConfig',
+        'django_revision.apps.AppConfig',
+        'edc_base.apps.AppConfig',
+        'edc_lab.apps.AppConfig',
+        'edc_protocol.apps.AppConfig',
+        'edc_device.apps.AppConfig',
+        'edc_identifier.apps.AppConfig',
+        'ambition_labs.apps.AppConfig',
+    ],
+    add_dashboard_middleware=True,
+    add_lab_dashboard_middleware=True,
+    # use_test_urls=True,
+).settings
+
+
+def main():
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+    django.setup()
+    failures = DiscoverRunner(failfast=True).run_tests(
+        [f'{app_name}.tests'])
+    sys.exit(failures)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
